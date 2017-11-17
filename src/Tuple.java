d80aec7e62e27c237063cb8b3d4dfcf6d91d9da8
ppate313 <32532135+ppate313@users.noreply.github.com>
2017-11-16 23:24:49 -0600

    Finished commenting

--- src/Tuple.java
+++ src/Tuple.java
@@ -1,6 +1,6 @@
 /*
  * File: Tuple.java
- * 		 This file contains the JMenubar for the grid.
+ * 		 his file used to get the index of cell.
  * 
  * 
  * Date: 11/16/2017
@@ -15,7 +15,7 @@
  * 		Name   : Ronald Trieu
  * 		Net ID : rtrieu2
  *  
- *  	Name   : Hend Khalil
+ *  	        Name   : Hend Khalil
  * 		Net ID : hkhali2
  * 
  * 		Name   : Priyank Patel

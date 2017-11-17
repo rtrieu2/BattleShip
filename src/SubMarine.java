9c63adecd2a8b3147237fa906e7e3bc19aef27bb
ppate313 <32532135+ppate313@users.noreply.github.com>
2017-11-16 23:23:50 -0600

    Finished commenting

--- src/SubMarine.java
+++ src/SubMarine.java
@@ -1,6 +1,6 @@
 /*
  * File: SubMarine.java
- * 		 This file contains the JMenubar for the grid.
+ * 		 This file place the SubMarine on board.
  * 
  * 
  * Date: 11/16/2017
@@ -15,7 +15,7 @@
  * 		Name   : Ronald Trieu
  * 		Net ID : rtrieu2
  *  
- *  	Name   : Hend Khalil
+ *  	        Name   : Hend Khalil
  * 		Net ID : hkhali2
  * 
  * 		Name   : Priyank Patel
